72911dff38f36f6aed781b9abe18f54caaa25e87
DigitalDetective47 <ninji2701@gmail.com>
2025-10-08 16:39:22 -0400

    add database fields from project proposal

---
+++ tetris/db/models.py
@@ -0,0 +1,36 @@
+from django.db.models import (
+    CASCADE,
+    CharField,
+    DateTimeField,
+    FloatField,
+    ForeignKey,
+    Model,
+    PositiveBigIntegerField,
+)
+
+
+class Shipment(Model):
+    id = PositiveBigIntegerField(primary_key=True)
+    name = CharField(max_length=64)
+
+
+class HandlingUnit(Model):
+    id = CharField(max_length=9, primary_key=True)
+    weight = FloatField()
+    x_size = FloatField()
+    y_size = FloatField()
+    z_size = FloatField()
+    shipment = ForeignKey(Shipment, CASCADE)
+    stop = CharField(max_length=3)
+
+
+class Simulation(Model):
+    time = DateTimeField()
+
+
+class SimPlacement(Model):
+    hu = ForeignKey(HandlingUnit, CASCADE)
+    x = FloatField()
+    y = FloatField()
+    z = FloatField()
+    sim = ForeignKey(Simulation, CASCADE)
