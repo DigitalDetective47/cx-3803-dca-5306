b33687c0c77da2ad2be77d6ad9da8bfd8e865f6e
Matthew <msebastian3304@gmail.com>
2025-10-19 12:53:55 -0400

    Fix merge conflicts

--- tetris/viewer/urls.py
+++ tetris/viewer/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path('api/get-simulations/', views.get_simulations, name='get_simulations'),
     # path('api/create-load/', views.create_load, name='create_load'),
     path('api/create-load-with-csv/', views.create_load_with_csv, name='create_load_with_csv'),
-    # path('api/import-csv/', views.import_csv, name='import_csv'),
     path('api/set-sim-position/', views.set_sim_position, name='set_sim_position'),
     path('api/get-simplacements/', views.get_simplacements, name='get-simplacements'),
-    path('api/import-csv/', views.import_csv, name='import_csv'),
+    # path('api/import-csv/', views.import_csv, name='import_csv'),
     path('api/delete-item/<int:item_id>/', views.delete_item, name='delete-item'),
 ]
